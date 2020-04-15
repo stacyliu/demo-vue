7402aaeaf73be084381753770e143b075a1a605c
liuyulan <liuyulan@bytedance.com>
2020-04-15 20:26:24 +0800

    add

---
+++ 12-前端模块化/02-ES6的模块化/aaa.js
@@ -0,0 +1,10 @@
+let name="aaaaa"
+let age=18
+flag=true
+function sum(m,n){
+    return m+n
+}
+
+if(flag){
+    sum(20,30)
+}
---
+++ 12-前端模块化/02-ES6的模块化/bbb.js
@@ -0,0 +1,10 @@
+let name="bbbbbb"
+let age=70
+flag=false
+function sum(m,n){
+    return m+n
+}
+
+if(flag){
+    sum(20,30)
+}
